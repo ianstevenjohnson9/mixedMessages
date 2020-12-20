15cc6b8aaa6fd1cd199cd2a487933dc8a7246eba
ian johnson <ianstevenjohnson@gmail.com>
2020-12-20 23:59:06 +0000

    mod html/script
    improved the layout of html and made slight changes to the script following the test.js file

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const greeting = ['Good morning', 'Good afternoon', 'Good evening'];
-const message = ["You always pass failure on the way to success.", "No one is perfect - that's why pencils have erasers.", "Winning doesn't always mean being first. Winning means you're doing better than you've done before.", "Once you replace negative thoughts with positive ones, you'll start having positive results", "Positive thinking will let you do everything better than negative thinking will.", "The only time you fail is when you fall down and stay down.", "If opportunity doesn't knock, build a door, knock knock!", "Success is the sum of small efforts repeated day in and day out.", "Happiness is the only thing that multiples when you share it", "Live life to the fullest and focus on the positive."];
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const exitMessage = [];
+const greetings = ['Good morning,', 'Good afternoon,', 'Good evening,'];
+const messages = ["You always pass failure on the way to success.", "You don't score until you score!", "No one is perfect - that's why pencils have erasers.", "Winning doesn't always mean being first, winning means you're doing better than you've done before.", "Once you replace negative thoughts with positive ones, you'll start having positive results.", "Positive thinking will let you do everything better than negative thinking will.", "The only time you fail is when you fall down and stay down.", "If opportunity doesn't knock, build a door, knock knock!", "Success is the sum of small efforts repeated day in and day out.", "Happiness is the only thing that multiples when you share it.", "Live life to the fullest and focus on the positive."];
+const leaveGreetings = ['Farewell my friend.', 'Have a great day.', 'Cheerio.'];
 
 
-let messageButton = document.getElementById("messageButton");
-let greetingMessage = document.getElementById("greetingMessage");
-let positiveMessage = document.getElementById("positiveMessage");
-let exitMessage = document.getElementById("exitMessage");
-let resetMessage = document.getElementById("resetMessage");
+const randomMessage = Math.floor(Math.random() * messages.length);
+const randomLeaveGreeting = Math.floor(Math.random() * leaveGreetings.length);
 
 
-// hidden elements
-greetingMessage.hidden = true;
-positiveMessage.hidden = true;
-exitMessage.hidden = true;
-resetMessage.hidden = true;
+console.log(messages[randomMessage]);
+console.log(leaveGreetings[randomLeaveGreeting]);
 
 
 
